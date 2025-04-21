485d6dee5fbf2521ca8036fef913a0d76471d016
Your Name <shahidalloor27@gmail.com>
2025-04-21 12:06:59 +0530

    first commit

---
+++ python_topics/test_markers.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+def test_str01():
+    num = 9/4
+    s1 = 'I like ' + 'python automation'
+    assert str(num) == '2.25'
+    assert s1 == 'I like python automation'
+    assert s1 + str(num) == 'I like python automation2.25'
+@pytest.mark.sanity
+def test_str02():
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    assert len(letters) == 26
+
+@pytest.mark.sanity
+def test_str03():
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    assert letters[0] == 'a'
+    assert letters[-1] == 'z' == letters[25]
